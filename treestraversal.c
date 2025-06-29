fccfa7c52d8b83b331130bdd93c794d06c7996e6
Tanisha Khanna <tanisha.khanna2022@gmail.com>
2025-06-29 16:29:00 +0530

    Create treestraversal.c

---
+++ treestraversal.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Inorder Traversal: Left -> Root -> Right
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+// Preorder Traversal: Root -> Left -> Right
+void preorder(struct Node* root) {
+    if (root == NULL) return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Postorder Traversal: Left -> Right -> Root
+void postorder(struct Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    // Creating a simple tree
+    //         1
+    //        / \
+    //       2   3
+    //      / \
+    //     4   5
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Inorder traversal: ");
+    inorder(root);
+
+    printf("\nPreorder traversal: ");
+    preorder(root);
+
+    printf("\nPostorder traversal: ");
+    postorder(root);
+
+    return 0;
+}
